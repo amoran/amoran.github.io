d21a036c014e63286d3303a99b22895ec32e5ae4
Adam Moran <amoran@cmu.edu>
2016-10-02 21:28:03 -0500

    init website

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      	<div>Adam Moran</div>
+      	<div>
+      		<div>Code</div>
+      		<div>Resume</div>
+      	</div>
+      </div>
+    );
+  }
+}
