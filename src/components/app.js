62bfcdcccdb7628a8851ee9d5fc284e1c8d7c43f
Adam Moran <amoran@cmu.edu>
2016-10-03 01:18:59 -0500

    feature: change design to terminal

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import Terminal from 'react-bash';
+import {structure} from '../util/constants.js';
+
+export const open = {
+  exec: ({structure, history, cwd}, command) => {
+    return {
+      structure,
+      cwd,
+      history: history.concat({value: `You called open on file ${command.args[0]}`})
+    };
+  }
+};
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-      	<div>Adam Moran</div>
-      	<div>
-      		<div>Code</div>
-      		<div>Resume</div>
-      	</div>
-      </div>
+	render() {
+		let extensions = {open};
+		return (
+			<div className="pageWrapper">
+				<div className="terminalWrapper">
+					<Terminal 
+						extensions={extensions}
+						history={[{value: 'Welcome!'}]}
+						prefix={'guest@amoran.github.io'}
+						structure={structure}
+					/>
+				</div>
+			</div>
     );
   }
 }
---
+++ src/util/constants.js
@@ -0,0 +1,23 @@
+export const structure = {
+    code: {
+        'Readme.md': { 
+        	content: 'Type help to list all commands.' 
+        },
+        'trecipes': {
+            file: { 
+            	'Readme.md': 'This is the text content for Readme in Trecipes' 
+            },
+        },
+        'andy-rover': {
+            file: { 
+            	'Readme.md': 'This is the text content for Readme in andy-rover' 
+            },
+        },
+        'groupme-insult-bot': {
+            file: { 
+            	'Readme.md': 'This is the text content for Readme in groupme-insult-bot' 
+            },
+        }
+    },
+    '.contact': { content: 'You can email me at amorancs@gmail.com' }
+};
